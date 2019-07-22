7a010263e1f11071f467bf04c6bf4cd0affde7c7
Milorad Pavlovic <pavlovicnikola511@gmail.com>
2019-07-22 09:22:55 +0200

    .

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -5,6 +5,7 @@ function EmptyCart() {
       <div className='container mt-4'>
          <div className='col-10 mx-auto, text-center text-title'>
             <h1>Your cart is currently empty</h1>
+            <h3>Add something in your wishlist</h3>
          </div>
       </div>
    )
